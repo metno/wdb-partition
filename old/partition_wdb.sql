094d9df3acaaefc09bc66fa64f9533f321314204
Vegard Bønes <vegard.bones@met.no>
2012-06-19 14:39:23 +0200

    Placed pre 1.5 files in a location where they are easy to find.

---
+++ old/partition_wdb.sql
@@ -0,0 +1,113 @@
+-- Replace all instances of TABLE_EXTENSION a proper name extenstion
+--                          TIME_FROM with correct from time
+--                          TIME_TO with correct to time
+
+
+BEGIN;
+
+\set ON_ERROR_STOP
+
+
+
+-- entry for lookup
+
+INSERT INTO wdb_partition.floatvalue_partitions VALUES (
+       'wdb_partition.floatvalue_TABLE_EXTENSION', 'TIME_FROM', 'TIME_TO');
+
+
+
+--table
+
+CREATE TABLE wdb_partition.floatvalue_TABLE_EXTENSION (CHECK (referencetime >= 'TIME_FROM' AND referencetime < 'TIME_TO')) INHERITS (wdb_int.floatvalue);
+
+
+
+-- permissions
+
+REVOKE ALL ON wdb_partition.floatvalue_TABLE_EXTENSION FROM public;
+GRANT ALL ON wdb_partition.floatvalue_TABLE_EXTENSION TO wdb_admin;
+GRANT SELECT, DELETE ON wdb_partition.floatvalue_TABLE_EXTENSION TO wdb_clean;
+
+
+
+-- indices
+
+ALTER TABLE ONLY wdb_partition.floatvalue_TABLE_EXTENSION ADD CONSTRAINT floatvalue_TABLE_EXTENSION_pkey PRIMARY KEY (valueid, valuetype);
+CREATE UNIQUE INDEX XAK1Wdb_FloatValue_TABLE_EXTENSION ON wdb_partition.FloatValue_TABLE_EXTENSION
+(
+       DataProviderId,
+       ReferenceTime,
+       DataVersion,
+       PlaceId,
+       ValueParameterId,
+       LevelParameterId,
+       LevelFrom,
+       LevelTo,
+       LevelIndeterminateCode,
+       ValidTimeFrom,
+       ValidTimeTo,
+       ValidTimeIndeterminateCode
+);
+
+CREATE INDEX i_FloatValue_TABLE_EXTENSION ON wdb_partition.FloatValue_TABLE_EXTENSION
+(
+       	dataproviderid, 
+       	referencetime, 
+       	placeid, 
+       	valueparameterid
+);
+
+
+
+-- foreign key constraints
+
+ALTER TABLE wdb_partition.FloatValue_TABLE_EXTENSION
+	ADD FOREIGN KEY (dataproviderid)
+					REFERENCES wdb_int.dataprovider
+					ON DELETE RESTRICT
+					ON UPDATE CASCADE;
+
+
+ALTER TABLE wdb_partition.FloatValue_TABLE_EXTENSION
+	ADD FOREIGN KEY (placeid)
+					REFERENCES wdb_int.placedefinition
+					ON DELETE RESTRICT
+					ON UPDATE CASCADE;
+
+
+ALTER TABLE wdb_partition.FloatValue_TABLE_EXTENSION
+	ADD FOREIGN KEY (validtimeindeterminatecode)
+					REFERENCES wdb_int.timeindeterminatetype
+					ON DELETE RESTRICT
+					ON UPDATE CASCADE;
+
+
+ALTER TABLE wdb_partition.FloatValue_TABLE_EXTENSION
+	ADD FOREIGN KEY (valueparameterid)
+					REFERENCES wdb_int.parameter
+					ON DELETE RESTRICT
+					ON UPDATE CASCADE;
+
+
+ALTER TABLE wdb_partition.FloatValue_TABLE_EXTENSION
+	ADD FOREIGN KEY (levelparameterid)
+					REFERENCES wdb_int.parameter
+					ON DELETE RESTRICT
+					ON UPDATE CASCADE;
+
+
+ALTER TABLE wdb_partition.FloatValue_TABLE_EXTENSION
+	ADD FOREIGN KEY (levelindeterminatecode)
+					REFERENCES wdb_int.levelindeterminatetype
+					ON DELETE RESTRICT
+					ON UPDATE CASCADE;
+
+
+ALTER TABLE wdb_partition.FloatValue_TABLE_EXTENSION
+	ADD FOREIGN KEY (confidencecode)
+					REFERENCES wdb_int.qualityconfidencecode
+					ON DELETE RESTRICT
+					ON UPDATE CASCADE;
+
+
+COMMIT;
---
+++ old/setup_partitioning.sql
@@ -0,0 +1,95 @@
+BEGIN;
+
+\set ON_ERROR_STOP
+
+CREATE SCHEMA wdb_partition;
+REVOKE ALL ON SCHEMA wdb_partition FROM PUBLIC;
+GRANT ALL ON SCHEMA wdb_partition TO wdb_admin;
+GRANT USAGE ON SCHEMA wdb_partition TO wdb_write;
+GRANT ALL ON SCHEMA wdb_partition TO wdb_clean;
+
+
+-- This table lists all partitions, with their time ranges
+CREATE TABLE wdb_partition.floatvalue_partitions (
+       partition_name text NOT NULL PRIMARY KEY,
+       fromtime timestamp with time zone NOT NULL,
+       totime timestamp with time zone NOT NULL
+);
+
+-- The distribution function
+CREATE OR REPLACE FUNCTION wdb_partition.floatvalue_allocate()
+RETURNS trigger AS
+$BODY$
+DECLARE
+	partition text;
+	insert_statement text;
+BEGIN
+	SELECT 
+	      partition_name INTO partition 
+	FROM 
+	      wdb_partition.floatvalue_partitions
+	WHERE 
+	      fromtime <= NEW.referencetime AND
+	      totime > NEW.referencetime;
+
+	IF NOT FOUND THEN
+	   RAISE EXCEPTION 'A partition is not yet created for time %', NEW.referencetime;
+	END IF;
+
+	-- Feil her
+	--INSERT INTO partition_name VALUES (NEW.*);
+	insert_statement := 'INSERT INTO '|| partition ||' VALUES ('|| 
+			 NEW.valueid || ', ' ||
+			 NEW.valuetype || ', ' ||
+			 NEW.dataproviderid || ', ' ||
+			 NEW.placeid || ', ' ||
+			 quote_literal(NEW.referencetime) || ', ' ||
+			 quote_literal(NEW.validtimefrom) || ', ' ||
+			 quote_literal(NEW.validtimeto) || ', ' ||
+			 NEW.validtimeindeterminatecode || ', ' ||
+			 NEW.valueparameterid || ', ' ||
+			 NEW.levelparameterid || ', ' ||
+			 NEW.levelfrom || ', ' ||
+			 NEW.levelto || ', ' ||
+			 NEW.levelindeterminatecode || ', ' || 
+			 NEW.dataversion || ', ' ||
+			 NEW.maxdataversion || ', ' ||
+			 NEW.confidencecode || ', ' ||
+			 NEW.value || ', ' ||
+			 quote_literal(NEW.valuestoretime) || ')'
+	;
+	EXECUTE insert_statement;
+
+	RETURN NULL;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER floatvalue_partition_allocate_trigger
+BEFORE INSERT ON wdb_int.floatvalue
+FOR EACH ROW EXECUTE PROCEDURE wdb_partition.floatvalue_allocate();
+
+
+
+-- Check that database settings are fit for using constraints
+CREATE OR REPLACE FUNCTION check_constraint_exclusion()
+RETURNS void AS
+$BODY$
+DECLARE
+	contraint_exclusion text;
+BEGIN
+	SELECT setting INTO contraint_exclusion 
+	FROM pg_settings 
+	WHERE name='constraint_exclusion';
+
+	IF contraint_exclusion != 'partition' THEN
+	   RAISE WARNING 'Constraint exclusion should probably be set to <partition>. (It is now <%>)', contraint_exclusion;
+	END IF;
+END;
+$BODY$
+LANGUAGE plpgsql;
+SELECT check_constraint_exclusion();
+DROP FUNCTION check_constraint_exclusion();
+
+
+COMMIT;
